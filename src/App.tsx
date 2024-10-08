355fd732bad89fb843d9f0ecd437a4834b8cf036
Morphosis <morphosisstartup@gmail.com>
2024-10-08 15:16:13 +0100

    Adicionado Roteamento

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import './App.css'
-import Footer from './components/Footer'
-import Guide from './components/Guide'
-import Header from './components/Header'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer";
+import Guide from "./components/Guide";
+import Header from "./components/Header";
+import Home from "./pages/Home";
 
 function App() {
-  
   return (
     <>
-      <Header />
-      <Guide />
-      <Footer />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import Footer from "../components/Footer";
+import Guide from "../components/Guide";
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Guide />
+      <Footer />
+    </>
+  );
+}
