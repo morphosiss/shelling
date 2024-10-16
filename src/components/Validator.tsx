06d957456c2c343b86bbb01bc3f128f5c8df441f
Mário Lino Salembe <linomario199010@gmail.com>
2024-10-16 11:13:07 +0100

    Adicionado Cadastro Simples

---
+++ src/components/Validator.tsx
@@ -0,0 +1,24 @@
+export const validateUserName = (value: string): string => {
+  if (/^\d/.test(value)) {
+    return "Seu username não pode começar com números";
+  } else if (value.length >= 1 && value.length < 3) {
+    return "Seu username deve ter no mínimo 3 caracteres";
+  } else if (value.length > 20) {
+    return "Seu username deve ter no máximo 20 caracteres";
+  }
+  return "";
+};
+
+export const validateEmail = (value: string): string => {
+  if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) && value.length > 0) {
+    return "Email inválido";
+  }
+  return "";
+};
+
+export const validatePass = (value: string): string => {
+  if (value.length < 8 && value.length > 0) {
+	return "Sua senha deve ter no mínimo 8 caracteres";
+  }
+  return "";
+}
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,8 +1,11 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import Logo from "/img/logo_white.png";
-import CountrySelect from '../components/ListCountries';
-import useSWR from "swr"
-import axios from "axios";
+import CountrySelect from "@/components/ListCountries";
+import {
+  validateUserName,
+  validateEmail,
+  validatePass,
+} from "../components/Validator";
 
 interface IInputGroup {
   typeData: "text" | "email" | "password";
@@ -11,6 +14,7 @@ interface IInputGroup {
   func?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   labelText: string;
   place: string;
+  textError?: string;
 }
 
 const InputGroup: React.FC<IInputGroup> = ({
@@ -19,7 +23,8 @@ const InputGroup: React.FC<IInputGroup> = ({
   id,
   labelText,
   place,
-  func
+  func,
+  textError,
 }) => {
   const InputRef = useRef<HTMLInputElement | null>(null);
 
@@ -34,24 +39,80 @@ const InputGroup: React.FC<IInputGroup> = ({
         name={nameData}
         id={id}
         onChange={func}
-        className="py-2.5 px-4 bg-[#2c2c2c] transition-all focus:border-green-500 rounded-lg border border-zinc-700 text-white outline-none"
+        className={`py-2.5 px-4 bg-[#2c2c2c] transition-all ${
+          textError?.length != 0
+            ? "focus:border-red-600"
+            : "focus:border-green-500"
+        } rounded-lg border ${
+          textError?.length != 0 ? "border-red-400" : "border-zinc-700"
+        } text-white outline-none`}
         placeholder={place}
       />
+      <small className="text-red-400">{textError}</small>
     </div>
   );
 };
 
 const Register: React.FC = () => {
-  const [errorUserName, setUserNameError] = React.useState<string>("");
-  const [showPassword, setShowPassword] = React.useState(false);
-  
+  const [errorUserName, setUserNameError] = useState<string>("");
+  const [errorEmail, setErrorEmail] = useState<string>("");
+  const [errorPass, setErrorPass] = useState<string>("");
+  const [valueCountry, setValueCountry] = useState<string>("");
+  const [isUserNameValid, setIsUserNameValid] = useState<boolean>(false);
+  const [isEmailValid, setIsEmailValid] = useState<boolean>(false);
+  const [isPassValid, setIsPassValid] = useState<boolean>(false);
+  const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>(true);
+  const [showPassword, setShowPassword] = useState(false);
+  const [loadCountry, setLoadCountry] = useState(true);
+
+  useEffect(() => {
+    if (
+      isUserNameValid &&
+      isEmailValid &&
+      isPassValid &&
+      valueCountry.length > 0
+    ) {
+      setIsButtonDisabled(false);
+    } else {
+      setIsButtonDisabled(true);
+    }
+  }, [isUserNameValid, isEmailValid, isPassValid, valueCountry]);
+
   const userNameVerification = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
-    if (value.length >= 1 && value.length < 3) {
-      setUserNameError("Seu username deve ter no mínimo 3 caracteres");
-    }
-    console.log(errorUserName);
-  }
+    const errorMessage = validateUserName(value);
+    setUserNameError(errorMessage);
+    setIsUserNameValid(errorMessage.length === 0 && value.length > 0);
+  };
+
+  const emailVerification = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    const errorMessage = validateEmail(value);
+    setErrorEmail(errorMessage);
+    setIsEmailValid(errorMessage.length === 0 && value.length > 0);
+  };
+
+  const validatePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    const errorMessage = validatePass(value);
+    setErrorPass(errorMessage);
+    setIsPassValid(errorMessage.length === 0 && value.length > 0);
+  };
+
+  const SendData = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+
+    const data = {
+      username: e.currentTarget.user_name.value,
+      email: e.currentTarget.email.value,
+      password: e.currentTarget.pass.value,
+      country: valueCountry,
+      };
+
+      
+    console.log(data);
+  };
 
   const ChangeTypeInput = () => {
     setShowPassword((prev) => !prev);
@@ -59,14 +120,25 @@ const Register: React.FC = () => {
 
   return (
     <main className="w-full paisagem-tablet:h-screen flex justify-center items-center">
-      <form className="bg-gradient-to-b paisagem-tablet:mt-0 retrato-tablet:mt-10 max-w-2xl w-full from-[#2c2c2c] to-transparent p-5 retrato-tablet:rounded-3xl shadow-3xl">
+      <form
+      method="POST"
+        onSubmit={SendData}
+        className="bg-gradient-to-b relative paisagem-tablet:mt-0 retrato-tablet:mt-10 max-w-2xl w-full from-[#2c2c2c] to-transparent p-5 retrato-tablet:rounded-3xl shadow-3xl"
+      >
+        {loadCountry && (
+          <div className="text-white absolute top-0 right-0 w-full h-full bg-[#242424] flex items-center justify-center z-30">
+            <span className="loader"></span>
+          </div>
+        )}
         <header className="text-center">
           <a href="/">
             <img src={Logo} alt="logo_image" className="w-28 m-auto" />
           </a>
-          <small className="text-zinc-200">Preocupa-mos com os seus dados!</small>
+          <small className="text-zinc-200">
+            Preocupa-mos com os seus dados!
+          </small>
         </header>
-        <div className="grid items-center retrato-tablet:grid-cols-2 grid-cols-1 mt-10 gap-x-2 gap-y-6">
+        <div className="grid items-start retrato-tablet:grid-cols-2 grid-cols-1 mt-10 gap-x-2 gap-y-6">
           <InputGroup
             labelText="Nome de Usuário"
             typeData="text"
@@ -74,23 +146,29 @@ const Register: React.FC = () => {
             func={userNameVerification}
             nameData="user_name"
             place="Crie um nome de usuário"
+            textError={errorUserName}
           />
           <InputGroup
             labelText="E-mail"
             typeData="email"
             id="email"
-           
+            func={emailVerification}
             nameData="email"
             place="Insira seu email"
+            textError={errorEmail}
           />
-          <CountrySelect key="country-select" />
+          <CountrySelect
+            setValueCountry={setValueCountry}
+            setCountryLoading={setLoadCountry}
+          />
           <InputGroup
-            
             labelText="Criar Senha"
             typeData={showPassword ? "text" : "password"}
             id="pass"
+            func={validatePassword}
             nameData="pass"
             place="Crie uma senha forte"
+            textError={errorPass}
           />
           <div className="flex items-center gap-2">
             <input type="checkbox" id="see_pass" onChange={ChangeTypeInput} />
@@ -99,10 +177,20 @@ const Register: React.FC = () => {
             </label>
           </div>
           <div className="retrato-tablet:col-span-2 flex justify-between">
-            <button className="px-6 transition-all hover:bg-zinc-800 hover:ring-4 hover:ring-zinc-500 hover:ring-opacity-25 font-medium py-2.5 text-white bg-zinc-900 rounded-full">
+            <button
+              type="button"
+              className="px-6 transition-all hover:bg-zinc-800 hover:ring-4 hover:ring-zinc-500 hover:ring-opacity-25 font-medium py-2.5 text-white bg-zinc-900 rounded-full"
+            >
               Criar conta
             </button>
-            <button className="px-6 transition-all hover:bg-green-700 hover:ring-4 hover:ring-green-500 hover:ring-opacity-25 font-medium py-2.5 text-white bg-green-600 rounded-full">
+            <button
+              disabled={isButtonDisabled}
+              className={`px-6 transition-all font-medium py-2.5 text-white bg-green-600 rounded-full ${
+                isButtonDisabled
+                  ? "opacity-50 cursor-not-allowed"
+                  : "hover:bg-green-700 hover:ring-4 hover:ring-green-500 hover:ring-opacity-25"
+              }`}
+            >
               Entrar
             </button>
           </div>
